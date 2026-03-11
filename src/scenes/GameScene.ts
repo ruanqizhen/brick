f705a65f24fdcfd9309390c027254ba37efad15a
ruanqizhen <ruanqizhen@gmail.com>
2026-03-10 21:48:58 -0400

    Update GameScene.ts

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -44,7 +44,7 @@ export class GameScene extends Phaser.Scene {
         if (data && typeof data.level === 'number') {
             this.currentLevelIndex = data.level;
         } else {
-            this.currentLevelIndex = 11; // 恢复从第一关开始
+            this.currentLevelIndex = 0; // 恢复从第一关开始
         }
         if (this.currentLevelIndex === 0) {
             this.lives = 3;
